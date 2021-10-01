3e8e13aad2f42a4a1a6df59c589cd6ac6fed554d
MainSilent <hamidreza.neo@gmail.com>
2021-10-01 14:50:17 +0330

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+ScriptPath=Null
+ServicePath=/etc/systemd/system/github_backup.service
+
+touch $ServicePath
+cat > $ServicePath << EOF
+[Unit]
+Description=Github Backup
+Wants=network.target
+After=network.target
+
+[Service]
+ExecStart=python3 $ScriptPath
+
+[Install]
+WantedBy=multi-user.target
+EOF
+echo "Service file Created"
+
+systemctl create github_backup.service
+systemctl start github_backup.service
+echo "Service Created"
+
+systemctl status github_backup.service
